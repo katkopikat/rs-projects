949e81b6aea806df86e17a5b8f7e858e9c22ec1e
Katy Rog <katkopikat@gmail.com>
2020-09-22 20:32:37 +0300

    feat: add sqrt func, add start zero value

--- calculator/script.js
+++ calculator/script.js
@@ -1,138 +1,149 @@
-let numbers = document.querySelectorAll('.data-number'),
-    operations = document.querySelectorAll('.data-operation'),
-    point = document.getElementById('point'),
-    ce = document.getElementById('ce'),
-    c = document.getElementById('c'),
-    result = document.getElementById('result'),
-    display = document.getElementById('current-operand'),
-    displayPrevious = document.getElementById('previous-operand'),
-    plusmin = document.getElementById('plusmin'),
-    MemoryCurrentNumber = "0",
-    MemoryNewNumber = false,
-    MemoryPendingOperation = '';
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
 
-for (let i = 0; i < numbers.length; i++) {
-    var numberBtn = numbers[i];
-    numberBtn.addEventListener('click', function (e) {
-        numberPress(e.target.textContent);
+    clear() {
+        this.currentOperand = '0';
+        this.previousOperandTextElement.innerText = '0';
+        this.operation = undefined;
+    }
 
-    });
-};
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
 
-for (let i = 0; i < operations.length; i++) {
-    var operationBtn = operations[i];
-    operationBtn.addEventListener('click', function (e) {
-        operationPress(e.target.textContent);
-    });
-};
-
-plusmin.addEventListener('click', function (e) {
-    plusminBtn(e.target.textContent);
-});
-
-point.addEventListener('click', function (e) {
-    pointBtn(e.target.textContent);
-});
-
-c.addEventListener('click', function (e) {
-    clear(e.target.id);
-});
-
-
-ce.addEventListener('click', function (e) {
-    clear(e.target.id);
-});
-
-
-function numberPress(number) {
-    if (MemoryNewNumber) {
-        display.value = number;
-        MemoryNewNumber = false;
-    } else {
-        if (display.value === "0") {
-            display.value = number;
+    plusmin() {
+        if (this.currentOperand.indexOf('-') === -1) {
+            this.currentOperand = '-' + this.currentOperand;
         } else {
-            display.value += number;
+            this.currentOperand = this.currentOperand.replace("-", "");
         }
     }
-}
 
+    addNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        if ((this.currentOperand.length >= 1 && this.currentOperand[0] === '0') ||
+            (this.currentOperand[0] === '-' && this.currentOperand[1] === '0')) {
+            this.currentOperand = this.currentOperand.toString().replace("0", "");
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
 
-function operationPress(op) {
-    let valueBeforeOperation = display.value;
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
 
-    if (MemoryNewNumber && MemoryPendingOperation !== "=") {
-        display.value = MemoryCurrentNumber;
-    } else {
-        MemoryNewNumber = true;
-        displayPrevious.value = MemoryCurrentNumber+MemoryPendingOperation;
-        if (MemoryPendingOperation === '+') {
-            MemoryCurrentNumber += parseFloat(valueBeforeOperation);
-        } else if (MemoryPendingOperation === '-') {
-            MemoryCurrentNumber -= parseFloat(valueBeforeOperation);
-        } else if (MemoryPendingOperation === '*') {
-            MemoryCurrentNumber *= parseFloat(valueBeforeOperation);
-        } else if (MemoryPendingOperation === '/') {
-            MemoryCurrentNumber /= parseFloat(valueBeforeOperation);
-        } else if (MemoryPendingOperation === '^') {
-            MemoryCurrentNumber = parseFloat(valueBeforeOperation * valueBeforeOperation);
-        } else if (MemoryPendingOperation === '\u221A') {
-            //MemoryCurrentNumber = parseFloat(Math.sqrt(valueBeforeOperation));
-            if (valueBeforeOperation >= 0) {
-                MemoryCurrentNumber = parseFloat(Math.sqrt(valueBeforeOperation));
-            } else {
-                display.value = "I can`t do it!"
-            }
+    sqrtFunc() {
+        if (+this.currentOperand < 0) {
+            this.currentOperand = 'отрицательных чисел';
+            this.previousOperandTextElement.innerText = 'и не изымай корень из';
+            this.operation = undefined;
         } else {
-            MemoryCurrentNumber = parseFloat(valueBeforeOperation);
-        }
-
-        display.value = parseFloat(MemoryCurrentNumber);
-        MemoryPendingOperation = op;
-        console.log(display.value);
-    }
-}
-
-
-
-function pointBtn(point) {
-    let valueBeforeOperation = display.value;
-
-    if (MemoryNewNumber) {
-        valueBeforeOperation = "0.";
-        MemoryNewNumber = false;
-    } else {
-        if (valueBeforeOperation.indexOf('.') === -1) {
-            valueBeforeOperation += '.';
+            let computation;
+            let current = parseFloat(this.currentOperand)
+            computation = Math.sqrt(current);
+            this.currentOperand = computation;
         }
     }
-    display.value = valueBeforeOperation;
-}
 
-function clear(id) {
-    if (id === 'ce') {
-        display.value = '0';
-        MemoryNewNumber = true;
-    } else if (id === 'c') {
-        display.value = '0';
-        displayPrevious.value = '0';
-        MemoryNewNumber = true;
-        MemoryCurrentNumber = 0;
-        MemoryPendingOperation = 0;
+
+    compute() {
+        let computation;
+        let prev = parseFloat(this.previousOperand);
+        let current = parseFloat(this.currentOperand);
+
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = (prev * 10000 + current * 10000) / 10000;
+                break;
+            case '-':
+                computation = (prev * 10000 - current * 10000) / 10000;
+                break;
+            case '/':
+                computation = ((prev * 10000) / 10000) / ((current * 10000) / 10000);
+                break;
+            case '*':
+                computation = ((prev * 10000) / 10000) * ((current * 10000) / 10000);
+                break;
+            case '^':
+                computation = Math.pow(prev, current);
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
-}
 
-function plusminBtn(plusmin) {
-    let valueBeforeOperation = display.value;
-    if (MemoryNewNumber) {
-        // valueBeforeOperation = "0.";
-        MemoryNewNumber = false;
-    } else {
-        if (valueBeforeOperation.indexOf('-') === -1) {
-            valueBeforeOperation = '-' + valueBeforeOperation;
-        } else {
-            valueBeforeOperation = valueBeforeOperation.replace("-", "");
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.previousOperand} ${this.operation}`;
         }
     }
-    display.value = valueBeforeOperation;
 }
+
+const numberBtns = document.querySelectorAll('.data-number'),
+    operationBtns = document.querySelectorAll('.data-operation'),
+    sqrtBtn = document.getElementById('sqrt'),
+    resultBtn = document.getElementById('result'),
+    delBtn = document.getElementById('del'),
+    allClearBtn = document.getElementById('c'),
+    plusminBtn = document.getElementById('plusmin'),
+    currentOperandTextElement = document.getElementById('current-operand'),
+    previousOperandTextElement = document.getElementById('previous-operand');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+resultBtn.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearBtn.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+
+})
+
+plusminBtn.addEventListener('click', button => {
+    calculator.plusmin();
+    calculator.updateDisplay();
+});
+
+delBtn.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+sqrtBtn.addEventListener('click', button => {
+    calculator.sqrtFunc();
+    calculator.updateDisplay();
+})
