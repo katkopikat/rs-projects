fc7b7ac9a50636dbc430e43a5949a76b7fb373f5
Katy Rog <katkopikat@gmail.com>
2020-11-25 19:06:07 +0300

    docs: install webpack server

--- gem-puzzle/webpack.config.js
+++ gem-puzzle/webpack.config.js
@@ -1,34 +1,69 @@
+/*  eslint linebreak-style: ["error", "windows"]  */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: 'index.js',
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'index_bundle.js'
-    },
-    plugins: [new HtmlWebpackPlugin()]
-  };
-  
-module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  mode: 'development',
+  entry: './src/main.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-      
-}
-    
-
-
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          outputPath: (file) => {
+            const imagesPath = file.split('src/')[1];
+            return imagesPath;
+          },
+        },
+      },
+      {
+        test: /\.(mp3|wav)$/i,
+        loader: 'file-loader',
+        options: {
+          name: './assets/sounds/[name].[ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.scss', '.mp3'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: 'src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+  output: {
+    publicPath: './',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
