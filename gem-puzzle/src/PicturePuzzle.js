b66474f4d4b8b0b0c55e18fec3d3f710d811b322
Katy Rog <katkopikat@gmail.com>
2020-11-25 19:09:00 +0300

    fix: fixed syntax by eslint rules - add works files

--- gem-puzzle/src/PicturePuzzle.js
+++ gem-puzzle/src/PicturePuzzle.js
@@ -1,68 +1,46 @@
+/* eslint-disable linebreak-style */
 import Cell from './Cell';
 import Game from './Game';
 
+export default class PicturePuzzle extends Game {
+  constructor(container, imageSrc, width, size, mode) {
+    super(container, width, size, mode);
+    this.imageSrc = imageSrc;
+    this.parentConteiner = container;
+    this.width = width;
+    this.height = width;
+    this.size = size;
+    this.mode = mode;
+    this.cells = [];
+    this.arrPosition = [];
+    this.countMoves = 0;
+    this.limitLeft = [];
+    this.limitRight = [];
+    this.allowBtnForClick = [];
 
-//     newGameBtn = document.querySelector('item--new_game'), 
-//     saveGameBtn = document.querySelector('item--save_game'), 
-//     scoresBtn = document.querySelector('item--scores'), 
-//     settingsBtn = document.querySelector('item--settings'), 
-//     rulesBtn = document.querySelector('item--rules');
+    this.init();
 
-    //moves = `Moves: ${countMoves}`;
-    //time = `Time: ${countMoves}:${countMoves}`;
+    const img = new Image();
+    img.onload = () => {
+      this.height = (img.height * this.width) / img.width;
+      this.container.style.width = `${this.width}rem`;
+      this.container.style.height = `${this.height}rem`;
+    };
 
-    //let moves = document.querySelector('moves');
-    
+    img.src = this.imageSrc;
+    this.generateCells();
+    this.emptyPos = +this.getEmptyPosition();
+    this.setLimite();
+    this.addClickable();
+    this.randomizeItem();
+    this.showSolve();
+  }
 
-//createEnvironment();
-export default class PicturePuzzle extends Game{
-    constructor(container, imageSrc, width, size, mode) {
-        super(container, width, size, mode);
-        this.imageSrc = imageSrc;
-        this.parentConteiner = container;
-        this.width = width;
-        this.height = width;
-        this.size = size;
-        this.mode = mode;
-        this.cells = [];
-        this.historyMoves = [];
-        this.historyMovesOrder = [];
-        this.historyMovesOrderEmpty = [];
-        this.arrPosition = [];
-        this.countMoves = 0;
-        this.limitLeft = [];
-        this.limitRight = [];
-        this.allowBtnForClick = [];
-
-      
-        this.init();
-
-        const img = new Image();
-        img.onload = () => {
-           this.height = img.height * this.width / img.width;
-            this.container.style.width = `${this.width}rem`;
-            this.container.style.height = `${this.height}rem`;
-           
-        };
-
-        img.src = this.imageSrc;
-        this.generateCells();
-        this.emptyPos = +this.getEmptyPosition();
-        this.setLimite();
-        this.addClickable();
-        this.randomizeItem();
-        this.showSolve();
+  generateCells() {
+    for (let i = 0; i < this.size * this.size; i += 1) {
+      this.cells.push(new Cell(this, i));
     }
-
-
-    generateCells() {
-        for (let i = 0; i < this.size * this.size; i++) {
-            this.cells.push(new Cell(this, i));
-        }
-         document.querySelector(`[data-id="${this.size*this.size}"]`).classList.add('empty');
-         //document.querySelector(`[data-id="${this.size*this.size}"]`).setAttribute('id','empty');
-        this.clickItems();
-     
-    }
+    document.querySelector(`[data-id="${this.size * this.size}"]`).classList.add('empty');
+    this.clickItems();
+  }
 }
-    
--- gem-puzzle/src/main.js
+++ gem-puzzle/src/main.js
@@ -1,43 +1,41 @@
+/* eslint-disable linebreak-style */
 import './style.css';
+import '../assets/sounds/move.mp3';
 import PicturePuzzle from './PicturePuzzle';
 import NumberPuzzle from './NumberPuzzle';
 import createEnvironment from './environment';
 
-let mode = '',
-    size = 4;
-
-createEnvironment();
-createNumberPuzzle()
-showSettings();
-newGame();
+let mode = '';
+let size = 4;
 
 function createNumberPuzzle() {
-    const numberPuzzle = new NumberPuzzle(
-        document.querySelector('.puzzle-wrapper'), 30, size, 'number');
-    return mode = 'number';
+  const numberPuzzle = new NumberPuzzle(
+    document.querySelector('.puzzle-wrapper'), 30, size, 'number',
+  );
+  mode = 'number';
+  return mode;
 }
 
 function createPictirePuzzle() {
-    const setRandomImg = (min, max) => {
-        return Math.floor(Math.random() * (max - min) + min);};
+  const setRandomImg = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-    let randomImg = setRandomImg(1, 151);
+  const randomImg = setRandomImg(1, 151);
 
-    const picturePuzzle = new PicturePuzzle(
-        document.querySelector('.puzzle-wrapper'),
-        `https://raw.githubusercontent.com/irinainina/image-data/master/box/${randomImg}.jpg`,
-        30, size, 'picture')
-
-    return  mode = 'picture';
+  const picturePuzzle = new PicturePuzzle(
+    document.querySelector('.puzzle-wrapper'),
+    `https://raw.githubusercontent.com/irinainina/image-data/master/box/${randomImg}.jpg`,
+    30, size, 'picture',
+  );
+  mode = 'picture';
+  return mode;
 }
 
 function showSettings() {
-    document.querySelector('.item--settings').addEventListener('click', () => {
-        let settings = document.createElement('div');
-        settings.classList.add('settings');
+  document.querySelector('.item--settings').addEventListener('click', () => {
+    const settings = document.createElement('div');
+    settings.classList.add('settings');
 
-        settings.innerHTML =
-            `<span class="corner">
+    settings.innerHTML = `<span class="corner">
             <span class="line line--horizontal"></span>
             <span class="line line--vertical"></span>
         </span>
@@ -65,85 +63,83 @@ function showSettings() {
             <span class="close__line close-line--vert"></span>
             <span class="close__line close-line--horiz"></span>
         </span>`;
-        document.querySelector('.header').after(settings);
+    document.querySelector('.header').after(settings);
 
+    setTimeout(() => {
+      const modePicture = document.querySelector('.mode--picture');
+      const modeNumber = document.querySelector('.mode--number');
+      const sizes = document.querySelectorAll('.size__item');
+      const soundOn = document.querySelector('.sound--on');
+      const soundOff = document.querySelector('.sound--off');
+      const audio = document.querySelector('.audio');
 
-        setTimeout(() => {
-            const mode_picture = document.querySelector('.mode--picture'),
-                mode_number = document.querySelector('.mode--number'),
-                sizes = document.querySelectorAll('.size__item'),
-                soundOn = document.querySelector('.sound--on'),
-                soundOff = document.querySelector('.sound--off'),
-                audio = document.querySelector('.audio');
+      sizes.forEach((item, index) => {
+        item.addEventListener('click', () => {
+          size = index + 3;
+          document.querySelector('.active_size').classList.remove('active_size');
+          item.classList.add('active_size');
+          document.querySelector('.puzzle').remove();
+          if (mode === 'number') {
+            createNumberPuzzle();
+          } else createPictirePuzzle();
+        });
+      });
 
-            sizes.forEach((item, index) => {
+      modeNumber.addEventListener('click', () => {
+        mode = 'number';
+        modePicture.classList.remove('active_mode');
+        modeNumber.classList.add('active_mode');
+        document.querySelector('.puzzle').remove();
+        createNumberPuzzle();
+        return mode;
+      });
 
-                item.addEventListener('click', () => {
-                    size = index + 3;
-                    document.querySelector('.active_size').classList.remove('active_size');
-                    item.classList.add('active_size');
-                    document.querySelector('.puzzle').remove();
-                    if (mode == 'number') {
-                        createNumberPuzzle();
-                    } else createPictirePuzzle();
-                })
-            })
+      modePicture.addEventListener('click', () => {
+        mode = 'picture';
+        modePicture.classList.add('active_mode');
+        modeNumber.classList.remove('active_mode');
+        document.querySelector('.puzzle').remove();
+        createPictirePuzzle();
+        return mode;
+      });
 
-            mode_number.addEventListener('click', () => {
-                mode = 'number';
-                mode_picture.classList.remove('active_mode');
-                mode_number.classList.add('active_mode');
-                document.querySelector('.puzzle').remove();
-                createNumberPuzzle();
-                return mode;
-            })
+      // sound off
+      soundOff.addEventListener('click', () => {
+        soundOn.classList.remove('active_sound');
+        soundOff.classList.add('active_sound');
+        audio.muted = true;
+        // this.audioOn = false;
+      });
 
-            mode_picture.addEventListener('click', () => {
-                mode = 'picture';
-                mode_picture.classList.add('active_mode');
-                mode_number.classList.remove('active_mode');
-                document.querySelector('.puzzle').remove();
-                createPictirePuzzle();
-                return mode;
-            })
+      // sound on
+      soundOn.addEventListener('click', () => {
+        soundOff.classList.remove('active_sound');
+        soundOn.classList.add('active_sound');
+        audio.muted = false;
 
-            //sound off
-            soundOff.addEventListener('click', () => {
-                soundOn.classList.remove('active_sound');
-                soundOff.classList.add('active_sound');
-                audio.muted = true;
-                //this.audioOn = false;
-            })
+        // this.audioOn = true;
+      });
 
-            //sound on
-            soundOn.addEventListener('click', () => {
-                soundOff.classList.remove('active_sound');
-                soundOn.classList.add('active_sound');
-                audio.muted = false;
-
-                // this.audioOn = true;
-            })
-
-            document.querySelector('.close-settings').addEventListener('click', () => {
-                settings.remove();
-            })
-
-        }, 500);
-
-        
-
-    })
+      document.querySelector('.close-settings').addEventListener('click', () => {
+        settings.remove();
+      });
+    }, 500);
+  });
 }
 
 function newGame() {
-    document.querySelector('.item--new_game').addEventListener('click', () => {
-        if (mode == 'number') {
-            document.querySelector('.puzzle').remove();
-            createNumberPuzzle();
-        } else {
-            document.querySelector('.puzzle').remove();
-            createPictirePuzzle();
-        }
-
-    })
+  document.querySelector('.item--new_game').addEventListener('click', () => {
+    if (mode === 'number') {
+      document.querySelector('.puzzle').remove();
+      createNumberPuzzle();
+    } else {
+      document.querySelector('.puzzle').remove();
+      createPictirePuzzle();
+    }
+  });
 }
+
+createEnvironment();
+createNumberPuzzle();
+showSettings();
+newGame();
